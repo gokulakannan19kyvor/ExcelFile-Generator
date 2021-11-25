81a88eafeb339daa7bce2bbd1959598a1d611cbe
Gokulak <gokulakannan@kyvorgenomics.com>
2021-11-25 15:19:18 +0530

    Demo file added

---
+++ demo.py
@@ -0,0 +1,75 @@
+# gene variance
+# gene evidences
+# clinical trials
+
+import os
+import pandas as pd
+from openpyxl import load_workbook
+folder = r"/home/gokulak/NewGene/MSK gene list - 660 genes"
+
+
+gv_total = pd.DataFrame()
+ge_total = pd.DataFrame()
+ct_total = pd.DataFrame()
+sheet6_total = pd.DataFrame()
+sheet5_total = pd.DataFrame()
+gene_summary_total = pd.DataFrame()
+gene_associated_trials_total = pd.DataFrame()
+variant_associated_tri_total = pd.DataFrame()
+gene_asso_total = pd.DataFrame()
+
+files = os.listdir(folder)
+# print(files)
+
+for file in files:
+    if file.endswith(".xlsx"):
+        # print(file)
+        excel_file = pd.ExcelFile(f"{folder}/{file}")
+        sheets = excel_file.sheet_names
+        # print(f"{file}, {sheets}")
+        for sheet in sheets:
+            # print(sheet)
+            if sheet.lower().strip() == "gene variants":
+                gv = excel_file.parse(sheet_name=sheet)
+                gv_total = gv_total.append(gv)
+                # print(gv_total.columns)
+
+            elif sheet.lower().strip() == "gene evidences" or sheet.lower() == "gene level evidences":
+                ge = excel_file.parse(sheet_name=sheet)
+                ge_total = ge_total.append(ge)
+
+            elif sheet.lower().strip() == "clinical trials":
+                ct = excel_file.parse(sheet_name=sheet)
+                ct_total = ct_total.append(ct)
+
+            elif sheet.lower().strip() == "sheet6":
+                sheet6 = excel_file.parse(sheet_name=sheet)
+                sheet6_total = sheet6_total.append(sheet6)
+
+            elif sheet.lower().strip() == "sheet5":
+                sheet5 = excel_file.parse(sheet_name=sheet)
+                sheet5_total = sheet5_total.append(sheet5)
+
+            elif sheet.lower().strip() == "gene summary":
+                gene_summary = excel_file.parse(sheet_name=sheet)
+                gene_summary_total = gene_summary_total.append(gene_summary)
+
+            elif sheet.lower().strip() == "gene associated trials (egfr ac":
+                gene_associated_trials = excel_file.parse(sheet_name=sheet)
+                gene_associated_trials_total = gene_associated_trials_total.append(
+                    gene_associated_trials)
+
+            elif sheet.lower().strip() == "variant associated clinical tri":
+                variant_associated_tri = excel_file.parse(sheet_name=sheet)
+                variant_associated_tri_total = variant_associated_tri_total.append(
+                    variant_associated_tri)
+
+# gv_total.to_excel(f"{folder}/geneVariants.xlsx", sheet_name="Gene Variants")
+# ge_total.to_excel(f"{folder}/geneEvidences.xlsx", sheet_name="Gene Evidences")
+# # ge_total.to_excel()
+# ct_total.to_excel(f"{folder}/clinicalTrials.xlsx",
+#                   sheet_name="Clinical Trials")
+
+with pd.ExcelWriter('combined660.xlsx') as writer:  # doctest: +SKIP
+    # gv_total.to_excel(writer, sheet_name='Gene Variants')
+    ge_total.to_excel(writer, sheet_name='Gene Evidences')
